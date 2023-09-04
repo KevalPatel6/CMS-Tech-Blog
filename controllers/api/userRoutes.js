199f6e9c1b0bdaa5b53072505a8fef6f9a3eb752
[Keval Patel] <[khp90611@gmail.com]>
2023-09-03 22:01:10 -0400

    finished the /api/user/singup route for posting to database

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post()
+//Login Route//
+router.post('/signup', async (req,res)=>{
+    try {
+        const signupUser = await User.create({
+            email: req.body.email,
+            user_name: req.body.user_name,
+            password: req.body.password,
+        })
+        res.status(200).json(dbUserData)
+        
+    } 
+    catch (error) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+
+    })
+
 
 router.get()
 
